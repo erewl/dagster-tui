f7e7249563ed9e9b8f5fe187ba884c320ea23add
erewl <grunert.katrin@protonmail.com>
2023-04-06 15:19:08 +0200

    Add helper class for aux functions

---
+++ helpers.go
@@ -0,0 +1,12 @@
+package main
+
+
+
+func filter(ss []string, cond func(string) bool) (ret []string) {
+    for _, s := range ss {
+        if cond(s) {
+            ret = append(ret, s)
+        }
+    }
+    return
+}
