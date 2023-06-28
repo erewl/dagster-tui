8e9c911d7667b29a91415bd9ea522572ae4f1beb
erewl <grunert.katrin@protonmail.com>
2023-06-28 16:15:10 +0200

    Remove unused func type

--- internal/helpers.go
+++ internal/helpers.go
@@ -13,8 +13,6 @@ func Filter[T any](ss []T, cond func(T) bool) (ret []T) {
     return
 }
 
-type Conv[T any] func (T) string
-
 func SortBy[T any](ss []T, c func(T) string) []T {
     sort.Slice(ss, func (i,j int) bool {
         return c(ss[i]) < c(ss[j])
